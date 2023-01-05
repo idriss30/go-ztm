5a98eb4676a9395df7ea43f8855a40827427913f
idriss30 <idriss.cissoko@hotmail.com>
2023-01-05 08:53:09 -0500

    using range to loop

--- slices/slices-exo.go
+++ slices/slices-exo.go
@@ -2,13 +2,24 @@ package main
 
 import "fmt"
 
-
 func printAssemblyLine(Content []string) {
 	for i := 0; i < len(Content); i++ {
 		item := i
 		fmt.Println("the current content is", Content[item])
 	}
 
+}
+
+func printStringWordsWithRange() {
+	mySlice := []string{"hello", "Go", "language"}
+	for i, element := range mySlice {
+		index := i
+		fmt.Printf("the word is %v and the index is %v\n", element, index)
+
+		for _, character := range element {
+			fmt.Printf("the character is %q\n", character) // if not adding the %q it will print the bite number of the rune and not the actual letter
+		}
+	}
 }
 func main() {
 	var assemblyContent = []string{"carts", "convoyeurs belts", "workers"}
@@ -19,5 +30,8 @@ func main() {
 	newAssemblyContent := assemblyContent[3:]
 	fmt.Println("############")
 	printAssemblyLine(newAssemblyContent)
+
+	fmt.Println("//////////////////// printing with range")
+	printStringWordsWithRange()
 
 }
